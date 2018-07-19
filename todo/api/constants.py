d7704d76990fec10ea6613dcc12ac691d66dd942
Dhruv Singh <dhruvsingh.er@gmail.com>
2018-07-19 20:05:06 +0530

    Add api to apps and add task model

---
+++ todo/api/constants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""contants for api."""
+from django.utils.translation import gettext_lazy as _
+
+NOT_DONE_STATE = 1
+DONE_STATE = 1
+
+DONE_TEXT = "Done"
+NOT_DONE_TEXT = "Not Done"
+
+TASK_STATE_CHOICES = (
+    (NOT_DONE_STATE, _(NOT_DONE_TEXT)),
+    (DONE_STATE, _(DONE_TEXT)),
+)
--- todo/api/models.py
+++ todo/api/models.py
@@ -1,3 +1,32 @@
+# -*- coding: utf-8 -*-
+"""api models."""
 from django.db import models
-
+from django.utils import timezone
+from .constants import TASK_STATE_CHOICES, NOT_DONE_STATE
 # Create your models here.
+
+
+class Task(models.Model):
+    """Task model."""
+
+    state = models.IntegerField(
+        default=NOT_DONE_STATE,
+        choices=TASK_STATE_CHOICES
+    )
+    text = models.TextField()
+    due_date = models.DateTimeField(
+        blank=True,
+        null=True
+    )
+    created_on = models.DateTimeField(
+        auto_now=True
+    )
+    updated_on = models.DateTimeField(
+        blank=True,
+        null=True
+    )
+
+    def save(self, *args, **kwargs):
+        """Update times."""
+        self.modified = timezone.now()
+        return super(Task, self).save(*args, **kwargs)
