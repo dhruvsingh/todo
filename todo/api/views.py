06d1bdceb75de35c4e8ed50ba42e5106f0db5cbb
Dhruv Singh <dhruvsingh.er@gmail.com>
2018-07-19 22:42:10 +0530

    Add django filters for filtering

--- todo/api/views.py
+++ todo/api/views.py
@@ -5,9 +5,13 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     """API endpoint that allows Task to be viewed, listed, added or edited."""
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backend = (DjangoFilterBackend,)
+    filter_fields = ('state', 'due_date')
