2eccef76471ae33ceb24f89b9a1a3dbadfa642c7
Dhruv Singh <dhruvsingh.er@gmail.com>
2018-07-19 22:29:07 +0530

    Add state display field

--- todo/api/serializers.py
+++ todo/api/serializers.py
@@ -8,9 +8,15 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     """Serializer class for Task model."""
 
+    state_display = serializers.SerializerMethodField()
+
     class Meta:
         """Meta for TaskSerializer."""
 
         model = Task
-        fields = ('state', 'text', 'due_date')
-        read_only_fields = ('id',)
+        fields = ('id', 'state', 'text', 'due_date', 'state_display')
+        read_only_fields = ('id', 'state_display')
+
+    def get_state_display(self, obj):
+        """State Display to show meaningful state."""
+        return obj.get_state_display()
